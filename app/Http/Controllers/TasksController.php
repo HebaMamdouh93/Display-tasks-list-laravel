e41c562f88b827626c0076508a97ce18cb507980
HebaMamdouh93 <hebamamdouh2016@gmail.com>
2018-05-27 12:07:52 +0200

    Make model and controller for tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function index()
+    {
+        $tasks=Task::All();
+        return view('tasks.home',[
+            'tasks' => $tasks,  
+        ]);
+    }
+}
